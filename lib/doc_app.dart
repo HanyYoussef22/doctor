bec7d882620af56f95e3efecaa2ef30162f80a1f
HanyYoussef <hanyy816@gmail.com>
2024-06-24 01:27:02 +0300

    -Finshed system Design -added flutter_SVG & flutter_Screen_util -aded doc_client_app -add theme file text & color -added onbording screen

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -1,10 +1,28 @@
+import 'package:doc_app/core/routing/app_router.dart';
+import 'package:doc_app/core/routing/roters.dart';
+import 'package:doc_app/core/theming/style/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DocApp extends StatelessWidget {
-  const DocApp({super.key});
+  final AppRouter appRouter;
+  const DocApp({super.key, required this.appRouter});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  ScreenUtilInit(
+      designSize: const Size(375,812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        title: 'Doctor',
+        theme: ThemeData(
+          primaryColor: ColorsManger.primaryColor,
+          scaffoldBackgroundColor: ColorsManger.mainBackgroundColor
+        ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: Routers.onBordingScreen,
+        onGenerateRoute: appRouter.generateRouter,
+      ),
+    );
   }
 }
